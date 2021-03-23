af48a5b85caa62a225dad9babc41cf62e2cf0a7f
Raissa Padua <raissa@raissas-mbp.myfiosgateway.com>
2021-03-23 15:00:10 -0400

    Fixed minor bug

--- src/components/add-to-list-form.jsx
+++ src/components/add-to-list-form.jsx
@@ -6,6 +6,10 @@ const AddEmailForm = () => {
     //declare the field(s) as an array form and assign it to an empty useState('')
     const [email, setEmail] = useState('')
 
+    //show error for email
+    const [errors, setErrors] = useState({})
+
+
     //submit button
     function onSubmit(e) {
         e.preventDefault() //prevents refreshing the app when button is clicked
@@ -28,7 +32,7 @@ const AddEmailForm = () => {
            <div>
            <input 
                 type="text" 
-                placeholder="email address"
+                placeholder="email address" required
                 value={email} //sets the value to the user's input
                 onChange={ e => setEmail(e.currentTarget.value)} //sets the current value of the email to setEmail
                 />
