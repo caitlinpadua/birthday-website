f8d577a957f6afca01f1e2db4426d6018c1c5b4d
Raissa Padua <raissa@raissas-mbp.myfiosgateway.com>
2021-03-30 13:47:08 -0400

    Fixed font size

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import mobile from './img/mobile.png'
 import mobile2 from './img/mobile2.png'
 import tablet2 from './img/tablet2.png'
 import AddEmailForm from './components/add-to-list-form'
+import './index.css'
 import './App.css';
 
 
@@ -13,7 +14,8 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className ="gradient-text">Cora @ 80</h1>
+        <h1 className ="gradient-text">Nanay Cora</h1>
+        <h2 className ="gradient-text">@80</h2>
         <p>COMING SOON</p>
         <AddEmailForm/>
       </div>
